e6d5673678e4d647b871da4764f76296c6b5265c
Markus Michalsky <markus.michalsky@klarna.com>
2023-05-16 13:50:26 +0200

    sass setup + clean dev deps

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref } from "vue";
 
 type Mighty = { x: string; y: number }
 
-const x: Mighty = { x: "test", y: 12 }
+const x: Mighty = { x: "test", y: 12 };
 
-const value = ref(0)
+const value = ref(0);
 </script>
+
 <template>
   <button @click="value++">this is a value: {{ value }} {{ JSON.stringify(x) }}</button>
 </template>
+
+<style lang="scss" scoped>
+$color: red;
+
+button {
+  color: $color;
+}
+</style>
